6c76f95df80ad9a4ce6017dfb9ec840d4a71c8d8
Aakash Varma <aakashvarma1@gmail.com>
2020-10-23 22:43:46 +0530

    Updates

---
+++ precache-manifest.ff433c2f3497eaca375759a4c66cad70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9431a05f986c966359ef5f9dd39a362b",
+    "url": "/gupshup.online/index.html"
+  },
+  {
+    "revision": "ae1f85a7f4b3039c1a1f",
+    "url": "/gupshup.online/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a6d5c8ee7c9d31470410",
+    "url": "/gupshup.online/static/js/2.941e787d.chunk.js"
+  },
+  {
+    "revision": "c3f07bce6d97df253cafb222d25fc17f",
+    "url": "/gupshup.online/static/js/2.941e787d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae1f85a7f4b3039c1a1f",
+    "url": "/gupshup.online/static/js/main.776346ee.chunk.js"
+  },
+  {
+    "revision": "86b750a0ff73de6a505f",
+    "url": "/gupshup.online/static/js/runtime-main.0d457302.js"
+  }
+]);
